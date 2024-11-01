93d28e7f4c43091c443b0103bffda92209bc398e
Medica-Group <medicagroupwebsite@gmail.com>
2024-11-01 12:48:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,63 +1,28 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "OPTIONS"]}})
+CORS(app)
 
 @app.route('/')
 def hello():
     return 'Hello, Looker!'
 
 @app.route('/analyze', methods=['GET'])
-def get_manifest():
-    manifest = {
-        "configParams": [],
-        "dateRanges": [],
-        "getAuthType": {
-            "type": "NONE"
-        },
-        "getConfig": {
-            "configParams": []
-        },
-        "getSchema": {
-            "schema": [
+def manifest():
+    return jsonify({
+        "name": "Simple Connector",
+        "version": "1",
+        "authType": "NONE",
+        "schema": {
+            "fields": [
                 {
                     "name": "value",
-                    "label": "Value",
-                    "dataType": "NUMBER",
-                    "semantics": {
-                        "conceptType": "METRIC"
-                    }
-                }
-            ]
-        },
-        "getData": {
-            "data": [
-                {
-                    "values": [100]
-                },
-                {
-                    "values": [200]
+                    "dataType": "NUMBER"
                 }
             ]
         }
-    }
-    
-    # Add CORS headers
-    response = jsonify(manifest)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-    return response
-
-@app.route('/analyze', methods=['POST'])
-def analyze():
-    return jsonify({
-        "data": [
-            {"value": 100},
-            {"value": 200}
-        ]
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run()
