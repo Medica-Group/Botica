91207904adfc6d58a7a1991dd638221da2de3c2d
Medica-Group <medicagroupwebsite@gmail.com>
2024-11-01 11:59:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,50 +2,62 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "OPTIONS"]}})
 
 @app.route('/')
 def hello():
     return 'Hello, Looker!'
 
-@app.route('/analyze')
+@app.route('/analyze', methods=['GET'])
 def get_manifest():
     manifest = {
-        "name": "Data Analysis Connector",
-        "version": "1.0",
-        "serviceType": "REST",
-        "authType": "NONE",
-        "endpoints": [{
-            "name": "getData",
-            "httpMethod": "POST",
-            "url": "/data"
-        }],
-        "schema": {
-            "type": "object",
-            "properties": {
-                "data": {
-                    "type": "array",
-                    "items": {
-                        "type": "object",
-                        "properties": {
-                            "dimension1": {"type": "string"},
-                            "metric1": {"type": "number"}
-                        }
+        "configParams": [],
+        "dateRanges": [],
+        "getAuthType": {
+            "type": "NONE"
+        },
+        "getConfig": {
+            "configParams": []
+        },
+        "getSchema": {
+            "schema": [
+                {
+                    "name": "value",
+                    "label": "Value",
+                    "dataType": "NUMBER",
+                    "semantics": {
+                        "conceptType": "METRIC"
                     }
                 }
-            }
+            ]
+        },
+        "getData": {
+            "data": [
+                {
+                    "values": [100]
+                },
+                {
+                    "values": [200]
+                }
+            ]
         }
     }
-    return jsonify(manifest)
+    
+    # Add CORS headers
+    response = jsonify(manifest)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    return response
 
-@app.route('/data', methods=['POST'])
-def get_data():
+@app.route('/analyze', methods=['POST'])
+def analyze():
     return jsonify({
         "data": [
-            {"dimension1": "A", "metric1": 100},
-            {"dimension1": "B", "metric1": 200}
+            {"value": 100},
+            {"value": 200}
         ]
     })
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
